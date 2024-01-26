26357ed42507a6681cf00ab121f1dde127afdbd4
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-01-26 16:41:55 +0200

    add my_pet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import My_pet from './components/my_pet';
+import data from "../src/db.json"
 
 function App() {
   return (
     <div className="App">
-      <My_pet />
+      <My_pet 
+      key={`My_pet_${data.id}`}
+      name={data.name}
+      description={data.description}
+      />
     </div>
   );
 }
--- src/components/my_pet.jsx
+++ src/components/my_pet.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import image_1 from "../assets/image_1.png"
+import image_2 from "../assets/image_2.png"
+import image_3 from "../assets/image_3.png"
+import image_4 from "../assets/image_4.png"
+import image_5 from "../assets/image_5.png"
+import image_6 from "../assets/image_6.png"
+import s from "../style/my_pet.module.css"
+import "../style/App.css"
 
-export default function My_pet() {
+export default function My_pet(props) {
   return (
-    <div>
+    <div className='container'>
+      <div className="img_container">
+        <img src={image_1} />
+        <div className="title_container">
+          <h1>Моя улюблена домашня тваринка</h1>
+          <p className=''>{props.name}</p>
+          <p className={s.desc}>{props.description}</p>
+        </div>
         
+      </div>
+         <div className='slider_container'>
+          <img src={image_2} alt="" />
+          <img src={image_3} alt="" />
+          <img src={image_4} alt="" />
+          <img src={image_5} alt="" />
+          <img src={image_6} alt="" />
+         </div>
     </div>
   )
 }
